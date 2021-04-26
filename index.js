9dcc2e63ebab28e977c8468891fb6a9e5d0d80f3
nick <nicholas_scalione@yahoo.com>
2021-04-26 11:03:32 -0400

    commit tree 6f903cf10540aeceb0f40a4d74dc2e271decee5d

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-/* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
+/* Given Code, don't edit */
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
@@ -10,4 +9,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const time = parseInt(timeString);
+  if (time < 12) return "Good Morning";
+  if (time <= 17 && time >= 12) return "Good Afternoon";
+  return "Good Evening";
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.querySelector('#greeting').innerText = string
+}
